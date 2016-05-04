3d3c1555fcb2a1c85c2e41b39dba8b36185a01f8
Amit Ashtekar <amit.ashtekar@synerzip.com>
2016-05-04 18:56:33 +0530

    state example implementation

--- app/js/controller/MainController.js
+++ app/js/controller/MainController.js
@@ -9,43 +9,39 @@ angular.module('SampleApp')
         '$scope','$window', '$state',
         function($scope,$window, $state) {
             $scope.transition = 'slide-left';
-            $scope.tabs = ['tab1', 'tab2', 'tab3', 'tab4'];
-            $scope.currentIndex = -1;
-            $scope.maxIndex = $scope.tabs.length;
-            $scope.left = false;
 
            // next();
 
-            function next()
+            function next(name)
             {
-                $scope.currentIndex = $scope.currentIndex+1 < $scope.maxIndex ? $scope.currentIndex+1 : 0;
-                var name = $scope.tabs[$scope.currentIndex];
+                //$scope.currentIndex = $scope.currentIndex+1 < $scope.maxIndex ? $scope.currentIndex+1 : 0;
+                //var name = $scope.tabs[$scope.currentIndex];
                 $state.go( name );
             }
 
-            $scope.left = function()
+            $scope.left = function(name)
             {
                 $scope.transition = 'slide-left';
-                next();
+                next(name);
             };
 
-            $scope.right = function()
+            $scope.right = function(name)
             {
                 $scope.transition = 'slide-right';
-                next();
+                next(name);
                // $state.go('tab3');
             };
 
-            $scope.top = function()
+            $scope.top = function(name)
             {
                 $scope.transition = 'slide-top';
-                next();
+                next(name);
             };
 
-            $scope.bottom = function()
+            $scope.bottom = function(name)
             {
                 $scope.transition = 'slide-bottom';
-                next();
+                next(name);
             };
 
         }
--- app/js/main.js
+++ app/js/main.js
@@ -19,33 +19,48 @@
                 url: '/tab1',
                 template: '<div class="tab tab1"><h1>Tab1 screen</h1></div>'
             })
-
             .state('tab2', {
                 name: 'tab2',
                 url: '/tab2',
                 template: '<div class="tab tab2"><h1>Tab2 screen</h1></div>'
             })
-
             .state('tab3', {
                 name: 'tab3',
                 url: '/tab3',
                 template: '<div class="tab tab3"><h1>Tab3 screen</h1></div>'
             })
-
             .state('tab4', {
                 name: 'tab4',
                 url: '/tab4',
                 template: '<div class="tab tab4"><h1>Tab4 screen</h1></div>'
             })
+
+
             .state('gotopage', {
                 url: '/gotopage',
                 template: '<h1>This Is A gotopage Page</h1>'
             })
-
             .state('gotopagewithparam', {
                 url: '/gotopagewithparam/:id/:location',
                 controller:'gotopageCtrl',
                 template: '<h1>This Is A gotopage with Following Params:id:{{id}} and location:{{location}}</h1>'
+            })
+
+
+            .state('home', {
+                url: '/home',
+                templateUrl: '../partials/partial1.html'
+            })
+            .state('home.one', {
+                url: '/one',
+                templateUrl: '../partials/partial2.html',
+                controller: function($scope) {
+                    $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                }
+            })
+            .state('home.two', {
+                url: '/two',
+                template: '<h1>Home Two state page</h1>'
             });
     }
   ]);
